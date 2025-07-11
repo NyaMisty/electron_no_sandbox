electron/electron

feat: add setAccentColor on Windows

shell/browser/native_window_views_win.cc
@@ -46,7 +46,7 @@ void SetWindowBorderAndCaptionColor(HWND hwnd, COLORREF color) {
     LOG(WARNING) << "Failed to set border color";
 }
 
-std::optional<DWORD> GetAccentColor() {
+std::optional<DWORD> GetSystemAccentColor() {
   base::win::RegKey key;
   if (key.Open(HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\DWM",
                KEY_READ) != ERROR_SUCCESS) {
@@ -583,7 +583,7 @@ void NativeWindowViews::UpdateWindowAccentColor() {
     if (!std::get<bool>(accent_color_))
       return;
 
-    std::optional<DWORD> accent_color = GetAccentColor();
+    std::optional<DWORD> accent_color = GetSystemAccentColor();
     if (!accent_color.has_value())
       return;
 

NyaMisty/electron_no_sandbox

shell/browser/native_window_views_win.cc
@@ -46,7 +46,7 @@ void SetWindowBorderAndCaptionColor(HWND hwnd, COLORREF color) {
     LOG(WARNING) << "Failed to set border color";
 }
 
-std::optional<DWORD> GetAccentColor() {
+std::optional<DWORD> GetSystemAccentColor() {
   base::win::RegKey key;
   if (key.Open(HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\DWM",
                KEY_READ) != ERROR_SUCCESS) {
@@ -594,7 +594,7 @@ void NativeWindowViews::UpdateWindowAccentColor() {
 
   // Use system accent color as fallback if no explicit color was set.
   if (!border_color.has_value() && should_apply_accent) {
-    std::optional<DWORD> system_accent_color = GetAccentColor();
+    std::optional<DWORD> system_accent_color = GetSystemAccentColor();
     if (system_accent_color.has_value()) {
       border_color = RGB(GetRValue(system_accent_color.value()),
                          GetGValue(system_accent_color.value()),